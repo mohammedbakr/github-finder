09c828c6831e3323fac4ea1cbf9f1e5a1b36542b
Mohamed Bakr <bakr.engineer@gmail.com>
2020-11-14 22:15:42 +0200

    converting the app to functional components using react hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-class Search extends Component {
-  state = {
-    text: ''
-  }
+const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
+  const [text, setText] = useState('')
 
-  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value })
+  const onChangeHandler = e => setText(e.target.value)
 
-  onSubmitHandler = e => {
+  const onSubmitHandler = e => {
     e.preventDefault()
-    if (!this.state.text) {
-      this.props.setAlert('Please enter something', 'light') 
+    if (!text.trim()) {
+      setAlert('Please enter something', 'light') 
     } else {
-      this.props.searchUsers(this.state.text)
-      this.setState({ text: '' })
+      searchUsers(text)
+      setText('')
     }
   }
 
-  render() {
-    const { showClear, clearUsers } = this.props
-
-    return (
-      <div>
-        <form
-          className="form"
-          onSubmit={this.onSubmitHandler}
-        >
-          <input
-            type="text"
-            name="text"
-            placeholder="Search Users..."
-            value={this.state.text}
-            onChange={this.onChangeHandler}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {showClear && <button
-          className="btn btn-light btn-block"
-          onClick={clearUsers}
-        >Clear</button>}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <form
+        className="form"
+        onSubmit={onSubmitHandler}
+      >
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChangeHandler}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && <button
+        className="btn btn-light btn-block"
+        onClick={clearUsers}
+      >Clear</button>}
+    </div>
+  )
 }
 
 Search.prototypes = {
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,93 +1,90 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 import Repos from '../repos/Repos'
 
-class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login)
-    this.props.getUserRepos(this.props.match.params.login)
+const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user
+
+  useEffect(() => {
+    getUser(match.params.login)
+    getUserRepos(match.params.login)
+    // eslint-disable-next-line
+  }, [])
+
+  if (loading) {
+    return <Spinner />
   }
 
-  render() {
-    const {
-      name,
-      company,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = this.props.user
-
-    const { loading, repos } = this.props
-
-    if (loading) {
-      return <Spinner />
-    }
-
-    return (
-      <React.Fragment>
-        <Link to="/" className="btn btn-light">
-          Back To Search
-        </Link>
-        Hireable: {''}
-        {hireable ? <i className="fas fa-check-circle text-success" /> :  <i className="fas fa-check-circle text-danger" />}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              alt="avatar..."
-              className="round-img"
-              style={{width: '150px'}}  
-            />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && <React.Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </React.Fragment>}
-            <a href={html_url} target="blank" className="btn btn-dark my-1">
-              Visit GitHub Profile
-            </a>
-            <ul>
-              <li>
-                {login && <React.Fragment>
-                    <strong>Username: </strong>{login}
-                  </React.Fragment>}
-              </li>
-              <li>
-                {company && <React.Fragment>
-                    <strong>Company: </strong>{company}
-                  </React.Fragment>}
-              </li>
-              <li>
-                {blog && <React.Fragment>
-                    <strong>Website: </strong>{blog}
-                  </React.Fragment>}
-              </li>
-            </ul>
-          </div>
+  return (
+    <React.Fragment>
+      <Link to="/" className="btn btn-light">
+        Back To Search
+      </Link>
+      Hireable: {''}
+      {hireable ? <i className="fas fa-check-circle text-success" /> :  <i className="fas fa-check-circle text-danger" />}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            alt="avatar..."
+            className="round-img"
+            style={{width: '150px'}}  
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-success">Following: {following}</div>
-          <div className="badge badge-light">Public Repos: {public_repos}</div>
-          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        <div>
+          {bio && <React.Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </React.Fragment>}
+          <a href={html_url} target="blank" className="btn btn-dark my-1">
+            Visit GitHub Profile
+          </a>
+          <ul>
+            <li>
+              {login && <React.Fragment>
+                  <strong>Username: </strong>{login}
+                </React.Fragment>}
+            </li>
+            <li>
+              {company && <React.Fragment>
+                  <strong>Company: </strong>{company}
+                </React.Fragment>}
+            </li>
+            <li>
+              {blog && <React.Fragment>
+                  <strong>Website: </strong>{blog}
+                </React.Fragment>}
+            </li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </React.Fragment>
-    )
-  }
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </React.Fragment>
+  )
 }
 
 User.propTypes = {
